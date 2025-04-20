2abe6d47b6b4c946c1312a78f5c6cd92deeda15e
Uttam Kharel <uttam.kharel@midastechnologies.com.np>
2025-04-20 16:49:23 +0545

    changes

--- run.py
+++ run.py
@@ -3,11 +3,19 @@ from app.services.open_ipo_scraper import scrape_open_ipo
 from app.services.open_fpo_scraper import scrape_open_fpo
 from app.services.open_dividend_scraper import scrape_open_dividend
 from app.services.open_right_scraper import scrape_open_right
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.main import app
 import uvicorn
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
